69edaf3d533c642ceeea730d5469d00b34206b6e
aidqen <idanmarkin8@gmail.com>
2025-04-07 12:29:44 +0300

    Add debounce to fetchMovies

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -13,6 +13,20 @@ const Search = () => {
   ), false)
 
   useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+
+        // Call updateSearchCount only if there are results
+
+      } else {
+        reset();
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
     <View>
       <Text>search</Text>
