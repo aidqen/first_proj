1a1309dd0eb3c706232d1318209ca3a56aa7be99
aidqen <idanmarkin8@gmail.com>
2025-04-07 12:30:51 +0300

    Add UI to the search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,3 +1,15 @@
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
+import React, { useEffect } from 'react'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
+import { useRouter } from 'expo-router'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
+import { icons } from '@/constants/icons'
+import SearchBar from '@/components/SearchBar'
+import { useState } from 'react'
+import { updateSearchCount } from '@/services/appwrite'
+
 const Search = () => {
   const router = useRouter()
   const [searchQuery, setSearchQuery] = useState('')
@@ -36,8 +48,60 @@ const Search = () => {
 
 
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
+      <FlatList data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className='px-5'
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: 'center',
+          gap: 16,
+          marginVertical: 16
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className='w-full flex-row justify-center mt-20 items-center'>
+              <Image source={icons.logo} className='w-12 h-10' />
+            </View>
+            <View className='my-5'>
+              <SearchBar
+                placeholder='Search movies...'
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)} />
+            </View>
+
+            {moviesLoading && (
+              <ActivityIndicator
+                size={'large'}
+                color='#0000ff'
+                className="my-3" />
+            )}
+            {moviesError && (
+              <Text className='text-red-500 text-center px-5 my-3'>
+                Error: {moviesError?.message}
+              </Text>
+            )}
+
+            {!moviesLoading && !moviesError && searchQuery.trim() && movies?.length || 0 > 0 && (
+              <Text className='text-xl text-white font-bold'>
+                Search Results for {' '}
+                <Text className='text-accent'>{searchQuery.trim()}</Text>
+              </Text>
+            )}
+          </>
+        }
+        ListEmptyComponent={
+          !moviesLoading && !moviesError ? (
+            <View className='mt-10 px-5'>
+              <Text className='text-center text-gray-500'>{searchQuery.trim() ? 'No movies found' : 'Search for a movie'}</Text>
+            </View>
+          ) : null
+        }
+      />
+
     </View>
   )
 }
